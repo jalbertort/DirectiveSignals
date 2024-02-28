4a85158926392ee90dd2d1a50349d98f5128600e
jalbertort <jramireztolentino@gmail.com>
2024-02-28 09:12:54 -0400

    Created folder shared and directive custom label

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+
+  constructor( private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el
+   }
+
+}
