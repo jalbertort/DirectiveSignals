b818c552e57c8bd00c90b8510876a4ad21e9acf8
jalbertort <jramireztolentino@gmail.com>
2024-02-29 13:08:06 -0400

    End seccion 23 - directives

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,14 +1,70 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
 })
-export class CustomLabelDirective {
+export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color:string = 'red';
+  private _errors?: ValidationErrors | null;
+
+  @Input() set color( value:string ) {
+    this._color = value
+    this.setStyle();
+  }
+
+  @Input() set errors( value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
 
   constructor( private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
+   }
+
+  ngOnInit(): void {
+    this.setStyle();
+  }
+
+   setStyle(): void {
+    if( !this.htmlElement ) return;
+    this.htmlElement!.nativeElement.style.color = this._color
+   }
+
+   changeColorRed():void {
+    this.htmlElement!.nativeElement.style.color = 'red'
+   }
+
+   setErrorMessage(): void {
+
+    if( !this.htmlElement ) return;
+    if( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if( errors.includes('required') ) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+
+    if( errors.includes('minlength') ) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const currentLength = this._errors!['minlength']['actualLength'];
+
+      this.htmlElement.nativeElement.innerText = `Mínimo ${currentLength}/${min} caracteres`;
+      return;
+    }
+
+    if( errors.includes('email') ) {
+      this.htmlElement.nativeElement.innerText = 'Correo invalido';
+      return;
+    }
+
    }
 
 }
